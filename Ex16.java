25d05afd99f21416477cd23c3c494145a0cc2f16
Sanketh B M <sankethbm333@gmail.com>
2026-06-08 13:59:35 +0530

    web class 06/06/26

--- Ex16.java
+++ Ex16.java
@@ -38,19 +38,36 @@ public class Ex16 {
         // }
         // System.out.println(Arrays.toString(arr));
 
-        // WAJP to get the min ele from the given array
+        // // WAJP to get the min ele from the given array
+
+        // int[] a = { 100, 250, 30, 400, 507, 60 };
+        // int min = a[0];
+
+        // for (int i = 0; i < a.length; i++) {
+        // if (a[i] < min) {
+        // min = a[i];
+        // }
+        // }
+
+        // System.out.println(min);
+
+        // WAJP to get the max ele from the given array
 
         int[] a = { 100, 250, 30, 400, 507, 60 };
-        int min = a[0];
+        int max = a[0];
 
         for (int i = 0; i < a.length; i++) {
-            if (a[i] < min) {
-                min = a[i];
+            if (a[i] > max) {
+                max = a[i];
             }
         }
 
-        System.out.println(min);
+        System.out.println(max);
 
+
+
+        // Assignment :
+        // 1. Min from 1st half and max from 2nd half
     }
 
 }
