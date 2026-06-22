415cfc1a1b1c4cf04c1debca1cfb30cf128224f5
Sanketh B M <sankethbm333@gmail.com>
2026-06-22 12:37:25 +0530

    web class 22/06/26

--- Ex22.java
+++ Ex22.java
@@ -1,15 +1,23 @@
 public class Ex22 {
+    @SuppressWarnings("empty-statement")
     public static void main(String[] args) {
         // 2D - ARRAYS
 
-
         /**
-         
+         * 
          * Whenever you want to store the data in tabular format then we use 2D array
-         * 2D array is used to store the in a tabular format which contains multiple rows and columns (matrix format)
+         * 2D array is used to store the in a tabular format which contains multiple
+         * rows and columns (matrix format)
          * 
          *
-         
-         * **/
+         * 
+         **/
+
+        int a[][] = { { 1, 2 }, { 3, 4 } };
+
+        System.out.println();
+        System.out.println(a[0]);
+        System.out.println(a[0].length);
+        System.out.println(a[0][0]);
     }
 }
